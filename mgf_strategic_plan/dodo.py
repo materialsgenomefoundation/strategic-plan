bdfea12383dd76235e5468b0fcc7fbbf32776d66
tonyfast <tony.fast@gmail.com>
2023-07-20 10:33:30 -0700

    add logos and favicon

--- mgf_strategic_plan/dodo.py
+++ mgf_strategic_plan/dodo.py
@@ -5,11 +5,15 @@ from shlex import split
 from shutil import rmtree
 from sys import executable
 import appdirs
+from doit.tools import create_folder
+
 
 HERE = Path(__file__).parent
 
+
 def rimraf(x):
     rmtree(X, True)
+
 
 def do(*x):
     from doit.tools import CmdAction
@@ -33,7 +37,7 @@ class X:
     TOC = HERE / "toc.yml"
     CONFIG = HERE / "config.yml"
     CONF = HERE / "conf.py"
-    RUN = F"conda run --live-stream -p {CONDA}"
+    RUN = f"conda run --live-stream -p {CONDA}"
     PDF_BUILD = DATA / "pdf"
     TEX = DATA / "pdf" / "MGF-Strategic-Plan.tex"
     PDF = DATA / "pdf" / "MGF-Strategic-Plan.pdf"
@@ -41,6 +45,8 @@ class X:
     CWD = Path().absolute()
     BUILD = HERE / "_build" / "html"
     PLANS = BUILD / "strategic-plan"
+    STATIC = HERE / "_static"
+
 
 def task_env():
     return dict(
@@ -62,49 +68,79 @@ def task_env():
             )
         ],
     )
+
+
 def task_configure():
     """build a pdf of the strategic document"""
     return dict(
-        actions=[do(F"""{X.RUN} jb config sphinx \
+        actions=[
+            do(
+                f"""{X.RUN} jb config sphinx \
                     --config {X.CONFIG.absolute()} \
                     --toc {X.TOC.absolute()} \
-                    {HERE.absolute()}""")],
+                    {HERE.absolute()}"""
+            )
+        ],
         verbosity=2,
         task_dep=["env"],
         targets=[X.CONF],
         file_dep=[X.TOC, X.CONFIG],
-        clean=True
+        clean=True,
     )
-    
+
+
 def task_pdf():
     """build a pdf of the strategic document"""
     yield dict(
         name="tex",
-        actions=[do(F"""{X.RUN} sphinx-build -b latex {HERE} {X.PDF_BUILD}""")],
+        actions=[do(f"""{X.RUN} sphinx-build -b latex {HERE} {X.PDF_BUILD}""")],
+        task_dep=["logo"],
         file_dep=[X.CONF],
         targets=[X.TEX],
-        clean=[(rimraf, [X.PDF_BUILD])]
+        clean=[(rimraf, [X.PDF_BUILD])],
     )
     yield dict(
         name="pdf",
         actions=[
-            F"{X.RUN} tectonic -X compile {X.TEX}",
-            F"cp {X.PDF} {X.PLANS}",
-            F'echo "copied {X.PLANS / X.PDF.name}"'
+            f"{X.RUN} tectonic -X compile {X.TEX}",
+            f"cp {X.PDF} {X.PLANS}",
+            f'echo "copied {X.PLANS / X.PDF.name}"',
         ],
         file_dep=[X.TEX],
-        clean=[(rimraf, [X.PDF_BUILD]), F"rm {X.PLANS / X.PDF.name}"],
-        targets=[X.PLANS / X.PDF.name]
+        clean=[(rimraf, [X.PDF_BUILD]), f"rm {X.PLANS / X.PDF.name}"],
+        targets=[X.PLANS / X.PDF.name],
     )
+
 
 def task_html():
     """build a pdf of the strategic document"""
     return dict(
-        actions=[do(F"""{X.RUN} jb build \
+        actions=[
+            do(
+                f"""{X.RUN} jb build \
                     --config {X.CONFIG.absolute()} \
                     --toc {X.TOC.absolute()} \
-                    {HERE.absolute()}""")],
+                    {HERE.absolute()}"""
+            )
+        ],
+        task_dep=["logo"],
         file_dep=[X.CONF],
         targets=[X.BUILD / "index.html"],
-        clean=[(rimraf, [X.BUILD])]
+        clean=[(rimraf, [X.BUILD])],
+    )
+
+
+def task_logo():
+    return dict(
+        targets=(
+            targets := [X.STATIC / f"logo-{sz}.png" for sz in [32, 128, 256, 512]]
+        ),
+        actions=[(create_folder, [X.STATIC])]
+        + [
+            do(
+                f"wget -O{x} https://avatars.githubusercontent.com/u/71715171?s={sz}&v=4"
+            )
+            for x, sz in zip(targets, [32, 128, 256, 512])
+        ],
+        uptodate=[x.exists for x in targets],
     )
